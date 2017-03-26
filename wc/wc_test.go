d7ef8db8ffde14b25d7c48255fba78951cef5956
Kashyap <kashyap.kmbc@gmail.com>
2017-03-27 01:38:16 +0530

    Add a basic benchmark script for the main counting routine

--- wc/wc_test.go
+++ wc/wc_test.go
@@ -71,3 +71,72 @@ func TestReadAndCount(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkReadAndCount(b *testing.B) {
+	input := ` Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+	diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+	erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+	ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+	ipsum dolor sit amet.
+
+	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+	tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+	vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+	no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+	diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+	erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+	ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+	ipsum dolor sit amet.
+
+	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+	tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+	vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+	no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+	diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+	erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+	ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+	ipsum dolor sit amet.
+
+	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+	tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+	vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+	no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+	diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+	erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+	ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+	ipsum dolor sit amet.
+
+	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+	tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+	vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+	no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+	diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+	erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+	ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+	ipsum dolor sit amet.
+
+	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+	tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+	vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+	no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+	diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+	erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+	ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+	ipsum dolor sit amet.
+
+	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+	tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+	vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+	no sea takimata sanctus est Lorem ipsum dolor sit amet.
+	`
+	reader := strings.NewReader(input)
+
+	for i := 0; i < b.N; i++ {
+		wc.ReadAndCount(reader)
+	}
+}
